8f5bfb7d337ea9cb6a075cadc4ced6ed0a27ebe0
Charlie Chao <charliechao@outlook.com>
2024-10-15 00:33:24 -0700

    Fix build script not wrapping path in quotes.

--- install-built.sh
+++ install-built.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 VAULT="$1"
 TARGET="$VAULT/.obsidian/plugins/optimage-kit"
-mkdir -p $TARGET
+mkdir -p "$TARGET"
 rm -f "$TARGET/main.js" "$TARGET/manifest.json"
 if [[ $2 == "-d" ]]; then
   # Create symbolic links
